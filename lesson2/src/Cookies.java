0ffd95165a333cb4f880a17fde88166ff55ae9ed
luisgjr08 <luisgjr08@gmail.com>
2025-11-06 16:54:27 -0600

    Add files via upload

---
+++ lesson2/src/Cookies.java
@@ -0,0 +1,23 @@
+public class Cookies {
+
+
+public Cookies(){
+
+    System.out.println("you haven't put a specific type of cookie");
+
+}
+
+public Cookies(String typeOfCookie){
+
+ System.out.println("You have selected a " + typeOfCookie + " cookie");
+}
+
+
+
+/*now try making your own method to call the class  with parameters
+enter your code below
+*/
+
+
+
+}
